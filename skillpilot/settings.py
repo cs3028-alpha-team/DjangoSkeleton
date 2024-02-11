010f401cfea155e9f7e541ee4026cbe730841e17
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-02-11 19:46:30 +0000

    change requirements file for deployment

--- skillpilot/settings.py
+++ skillpilot/settings.py
@@ -111,7 +111,6 @@ else:
         }
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -129,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
