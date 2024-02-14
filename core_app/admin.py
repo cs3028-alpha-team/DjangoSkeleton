3e3a0619ef2e483160d3f2b8f4dde738699eaf1e
Ella Thomson <ellalauriethomson@gmail.com>
2024-02-14 10:52:21 +0000

    add and register Models

--- core_app/admin.py
+++ core_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Students
+from .models import Internships
 
 # Register your models here.
+
+admin.site.register(Students)
+admin.site.register(Internships)
+
