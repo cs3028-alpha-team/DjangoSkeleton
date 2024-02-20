0cbd8b0a447c52c2901e2ceabec5b61acde02785
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-02-20 08:17:47 +0000

    completed all tests except integration, identified cause of pipeline failure

--- core_app/clean_data.py
+++ core_app/clean_data.py
@@ -3,6 +3,41 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Your round_to_closest function
+def round_to_closest(x=60): # default score if not provided is the passing score, i.e. 60%
+    # extract the unitary digit
+    unitary = int(x) % 10
+        
+    # extract the decimal digit
+    decimal = int(x)//10
+        
+    # round up or down based on the unitary digit
+    if unitary < 5: return decimal * 10
+    if unitary == 5: return x
+    else: return (decimal + 1) * 10
+
+
+# Your clean function
+def clean(program):
+    program = program.split("-")
+    study_mode, study_pattern = program[0], program[1]
+    
+    if study_mode == "N/A" and study_pattern == "N/A":
+        # assume student is campus, fulltime
+        return "Campus-FT"
+    
+    if study_mode == "N/A":
+        # assume student in on-campus
+        return f"Campus-{study_pattern}"
+    
+    if study_pattern == "N/A":
+        # assume student is fulltime
+        return f"{study_mode.capitalize()}-FT"
+    
+    # both entries are specified
+    return f"{study_mode.capitalize()}-{study_pattern}"
+
+
 def process_data():
     # Load jobs and candidates data
     jobs = pd.read_csv('data/jobs.csv')
@@ -20,51 +55,19 @@ def process_data():
     # Create the "StudyProgram" column by concatenating "StudyMode" and "StudyPattern"
     candidates['StudyProgram'] = candidates[["StudyMode", "StudyPattern"]].agg('-'.join, axis=1)
 
-    # Your clean function
-    def clean(program):
-        program = program.split("-")
-        study_mode, study_pattern = program[0], program[1]
-        
-        if study_mode == "N/A" and study_pattern == "N/A":
-            # assume student is campus, fulltime
-            return "Campus-FT"
-        
-        if study_mode == "N/A":
-            # assume student in on-campus
-            return f"Campus-{study_pattern}"
-        
-        if study_pattern == "N/A":
-            # assume student is fulltime
-            return f"{study_mode.capitalize()}-FT"
-        
-        # both entries are specified
-        return f"{study_mode.capitalize()}-{study_pattern}"
-
     # Drop the unified columns
     candidates.drop(["StudyMode", "StudyPattern"], axis=1, inplace=True)
 
     # Handle null-values in the StudyProgram column
     candidates["CleanedStudyProgram"] = candidates["StudyProgram"].apply(clean)
 
-    print("\n'candidates' dataframe after cleaning:")
-    print(candidates.head())
-
-    # Your round_to_closest function
-    def round_to_closest(x=60): # default score if not provided is the passing score, i.e. 60%
-        # extract the unitary digit
-        unitary = int(x) % 10
-            
-        # extract the decimal digit
-        decimal = int(x)//10
-            
-        # round up or down based on the unitary digit
-        if unitary < 5: return decimal * 10
-        if unitary == 5: return x
-        else: return (decimal + 1) * 10
-
     # Round the values in the MinScore column to the nearest 5%
     jobs['MinScore'] = jobs['MinScore'].apply(round_to_closest)
     print(jobs.head())
+
+    print("\n'candidates' dataframe after cleaning:")
+
+    print(candidates.head())
 
 # TO BE DELETED LATER-- BELOW
 
--- core_app/tests/test_views.py
+++ core_app/tests/test_views.py
@@ -4,53 +4,53 @@ from ..models import Student, Internship
 from ..forms import StudentForm, InternshipForm
 import json 
 
-# class TestViews(TestCase):
+class TestViews(TestCase):
     
-#     # this method runs before all other in the class, recognised by Django by default
-#     def setUp(self):
-#         self.client = Client()
-#         self.submit_student_url = reverse('submit-student')
-#         self.submit_internship_url = reverse('submit-internship')
-#         self.matching_view_url = reverse('matching')
+    # this method runs before all other in the class, recognised by Django by default
+    def setUp(self):
+        self.client = Client()
+        self.submit_student_url = reverse('submit-student')
+        self.submit_internship_url = reverse('submit-internship')
+        self.matching_view_url = reverse('matching')
 
-#     # test student form submission renders the correct output - submit_student View
-#     def test_submit_student_POST(self):
-#         form = {
-#             'fullname' : 'John John', 
-#             'course' : 'Nursing', 
-#             'score' : 79, 
-#             'experience' : 'lots of nursing experience', 
-#             'study_mode' : 'full-time', 
-#             'study_pattern' : 'on-campus'
-#         }
+    # test student form submission renders the correct output - submit_student View
+    def test_submit_student_POST(self):
+        form = {
+            'fullname' : 'John John', 
+            'course' : 'Nursing', 
+            'score' : 79, 
+            'experience' : 'lots of nursing experience', 
+            'study_mode' : 'full-time', 
+            'study_pattern' : 'on-campus'
+        }
 
-#         # simulate a POST request to the given url, and assert the output matches the expected
-#         response = self.client.post(self.submit_student_url, form)
-#         self.assertContains(response, 'Form submitted')
+        # simulate a POST request to the given url, and assert the output matches the expected
+        response = self.client.post(self.submit_student_url, form)
+        self.assertContains(response, 'Form submitted')
 
-#     # test student form submission rendenrs the correct output - submit_student View
-#     def test_submit_internship_POST(self):
-#         form = {
-#             'title' : 'Junior Doctor', 
-#             'company' : 'NHS', 
-#             'field' : 'Medicine', 
-#             'min_score' : 75, 
-#             'positions' : 5, 
-#         }
+    # test student form submission rendenrs the correct output - submit_student View
+    def test_submit_internship_POST(self):
+        form = {
+            'title' : 'Junior Doctor', 
+            'company' : 'NHS', 
+            'field' : 'Medicine', 
+            'min_score' : 75, 
+            'positions' : 5, 
+        }
 
-#         # simulate a POST request to the given url, and assert the output matches the expected
-#         response = self.client.post(self.submit_internship_url, form)
-#         self.assertContains(response, 'Form submitted')
+        # simulate a POST request to the given url, and assert the output matches the expected
+        response = self.client.post(self.submit_internship_url, form)
+        self.assertContains(response, 'Form submitted')
 
-#     # test the matching View
-#     def test_matching_view_POST(self):
-#         # carry out a POST and a GET request on the given url
-#         success_response = self.client.post(self.matching_view_url, data={})
-#         failure_response = self.client.get(self.matching_view_url)
+    # test the matching View
+    def test_matching_view_POST(self):
+        # carry out a POST and a GET request on the given url
+        success_response = self.client.post(self.matching_view_url, data={})
+        failure_response = self.client.get(self.matching_view_url)
 
-#         # expected output is a success response and a failure one, caused by the GET method
-#         self.assertContains(success_response, 'Matching process completed. Compatibility matrix saved to CSV file.')
-#         self.assertContains(failure_response, 'Error: POST request expected.')
+        # expected output is a success response and a failure one, caused by the GET method
+        self.assertContains(success_response, 'Matching process completed. Compatibility matrix saved to CSV file.')
+        self.assertContains(failure_response, 'Error: POST request expected.')
 
     
 class TestTemplatesContents(TestCase):
